31c4fe72dd4ff942bbee5359ca46bdc4157b0e8d
geffzhang <geffzhang@qq.com>
2018-08-28 16:31:50 +0800

    add exceptions

---
+++ src/Tars.Net.Codecs/Exceptions/TarsDecodeException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tars.Net.Codecs
+{
+    public class TarsDecodeException : Exception
+    {
+        public TarsDecodeException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
