63ffe85a59578deb99cef3fdf4569cd9087555cc
Victor.X.Qu <fs7744@hotmail.com>
2018-10-08 21:33:29 +0800

    add ut

---
+++ test/Tars.Net.Test/TarsStructAttributeTest.cs
@@ -0,0 +1,16 @@
+﻿using Tars.Net.Codecs.Attributes;
+using Xunit;
+
+namespace Tars.Net.Test
+{
+    public class TarsStructAttributeTest
+    {
+        [Fact]
+        public void ShouldEqualExpect()
+        {
+            Assert.Equal("Test", new TarsStructAttribute() { Comment = "Test" }.Comment);
+            Assert.Equal("Test2", new TarsStructPropertyAttribute(3) { Comment = "Test2" }.Comment);
+            Assert.Equal(13, new TarsStructPropertyAttribute(13) { Comment = "Test2" }.Order);
+        }
+    }
+}
